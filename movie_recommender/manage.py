907a58e450294c21e80c29581c967a290ec7fc95
deorhan <deorhan@edu.aau.at>
2025-06-01 15:11:43 +0200

    initial django setup

---
+++ movie_recommender/manage.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movie_recommender.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
+
+
+# Windows User: "env/Scripts/Activate.ps1" if you get an error try "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser" and execute the first command again
+# Mac User: source/env/Scripts/activate
+
+# "python manage.py runserver" to start the server
