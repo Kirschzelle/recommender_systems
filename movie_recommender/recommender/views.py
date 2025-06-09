d1d2cf22a86fd32a68a9e3bdbcf115ae986efded
deorhan <deorhan@edu.aau.at>
2025-06-09 15:59:46 +0200

    able to search movies

--- movie_recommender/recommender/views.py
+++ movie_recommender/recommender/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from .models import Movie
 
 # Create your views here.
 def home_view(request):
-    query_dict = dict(request.GET)
-    query = query_dict.get("q")
+    query = request.GET.get("q")
+    movie_objects = None
+    if query:
+        movie_objects = Movie.objects.filter(title__icontains = query)
     context = {
-        "movie_name": query
+        "query": query,
+        "movie_objects": movie_objects
     }
     return render(request, "home.html", context)
