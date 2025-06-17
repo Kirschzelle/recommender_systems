c2fd1b9aec6aa2672c77296573c0937692d33606
lukas <lukaskr@edu.aau.at>
2025-06-18 00:22:39 +0200

    Added methods to retrieve data to ready method

--- movie_recommender/recommender/apps.py
+++ movie_recommender/recommender/apps.py
@@ -1,6 +1,15 @@
-from django.apps import AppConfig
+import os
 
+from django.apps import AppConfig
+from extract_information_from_json_to_csv import extract_information_from_json_to_csv
+from movie_recommender.settings import BASE_DIR
+from data import Data
 
 class RecommenderConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'recommender'
+
+    def ready(self):
+        if not os.path.exists(os.path.join(BASE_DIR, "datasets/additional_data.csv")):
+            extract_information_from_json_to_csv(os.path.join(BASE_DIR, "datasets/extracted_content_ml-latest"))
+            data = Data()
