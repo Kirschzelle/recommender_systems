becb6e30b70744e471b0fdee59733f1bc3310110
lukas <lukaskr@edu.aau.at>
2025-06-23 01:39:59 +0200

    Changed data so only the movies from folder "information" are used

--- movie_recommender/data.py
+++ movie_recommender/data.py
@@ -8,22 +8,19 @@ from movie_recommender.settings import BASE_DIR
 class Data:
     instance = None
 
-    movies_df = None
     additional_data_df = None
-    movies_with_people_df = None
 
     genre_cosine_similarity_matrix = None
     collaborators_cosine_similarity_matrix = None
 
     def load_data(self):
-        self.movies_df = pd.read_csv(os.path.join(BASE_DIR, "datasets/ml-20m/movies.csv"))
-        self.movies_df['genres'] = self.movies_df['genres'].apply(lambda x: x.split('|'))
-
         self.additional_data_df = pd.read_csv(os.path.join(BASE_DIR, "datasets/additional_data.csv"))
 
-        self.movies_with_people_df = pd.merge(self.movies_df, self.additional_data_df, how='left', on='movieId')
-        self.movies_with_people_df["names"] = self.movies_with_people_df["names"].apply(
+        self.additional_data_df["names"] = self.additional_data_df["names"].apply(
             lambda x: x if pd.notna(x) else ["NO_DATA_AVAILABLE"])
+        self.additional_data_df["genres"] = self.additional_data_df["genres"].apply(
+            lambda x: x if pd.notna(x) else ["NO_DATA_AVAILABLE"]
+        )
 
     def precompute(self):
         self.load_data()
@@ -31,13 +28,13 @@ class Data:
         self.collaborators_cosine_similarity_matrix = self.calculate_cosine_similarity_matrix("names")
 
     def calculate_cosine_similarity_matrix(self, attr):
-        if self.movies_with_people_df is None:
+        if self.additional_data_df is None:
             raise Exception("Movies data not loaded")
 
         # generate a matrix of size #movies x #genres
         # it contains 0 and 1 indicating whether the genre applies to the movie
         mlb = MultiLabelBinarizer()
-        matrix = mlb.fit_transform(self.movies_with_people_df[attr])
+        matrix = mlb.fit_transform(self.additional_data_df[attr])
 
         # generate a matrix of size #movies x #movies
         # it contains the similarity-values for every movie combination
